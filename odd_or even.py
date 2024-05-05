8e9286b13fa81191507d12dcb02b1e2f1cf81869
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-05-05 15:03:52 +0530

    Add files via upload

---
+++ odd_or even.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the Number:"))
+if num>0:
+    if num % 2 == 0:
+        print("It's an Even number!")
+    elif num % 3 ==0:
+        print("It's an Odd number!")
+else:
+    print("invalid Input...")
