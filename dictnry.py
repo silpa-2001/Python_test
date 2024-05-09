a78480b2e3a58fc84badb53266325f130feb498c
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-05-09 15:38:22 +0530

    Add files via upload

---
+++ dictnry.py
@@ -0,0 +1,16 @@
+d={
+    "name":["virat","rohit","dhoni","sachin","jadeja","yuvraj","abd","raina","surya","abhishek"],
+    "age":[35,36,43,50,35,38,38,32,28,22],
+    "adress":["delhi","mumbai","ranchi","mumbai","gujarat","punjab","south africa","up","chennai","kerala"],
+    "sport":"cricket"
+}
+#print(d)
+print(d["sport"])
+print(len(d))
+y= dict(drama="lovely runner",character="im sol",date=8042024)
+print(y)
+print(y.get("drama"))
+print(y.keys())
+print(y.values())
+y["genre"]="rom-com"
+print(y)
