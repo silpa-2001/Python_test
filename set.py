28a7b63fdab863a6d33dad721318601db32c9a01
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-05-09 15:37:47 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,15 @@
+sets={"dog","rabbit","cat"}
+myset=set(("wolf","fox","coyote","rino"))
+set2={"kitten","puppy","panda"}
+"""print(sets)
+print(type(sets))
+print(myset)
+myset=myset.union(sets)
+myset.add("tiger")
+myset.update(set2)
+for i in myset:
+    print(i)
+myset.remove("fox")
+myset.discard("rino")
+myset.pop()"""
+print(myset)
