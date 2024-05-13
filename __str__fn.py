e0714d29c66c544747f724537a075ab6f09dc236
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-05-13 15:28:09 +0530

    Add files via upload

---
+++ __str__fn.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        #return f"{self.name}({self.age})"
+        return f"{self.name}  {self.age}"
+
+    
+obj=Person("Alex",21)
+print(obj)
