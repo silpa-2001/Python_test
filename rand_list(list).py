2bcc0a8a4e11319fee31d5bb085c559c2a39bb06
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-06-13 15:10:16 +0530

    Add files via upload

---
+++ rand_list(list).py
@@ -0,0 +1,7 @@
+import random
+
+num=[[3,21,26],[54,32,8],[9,24,13]]
+n=[0,1,2]
+print("Number matrix:\n",num)
+result=random.choice(num[random.choice(n)])
+print("Random number from the list of list:\n",result)
