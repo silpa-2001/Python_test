0893d6a2b8971ab77cffcb8ef190e8de6e94702f
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-06-26 16:47:33 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,24 @@
+#!C:\Users\Admin\AppData\Local\Programs\Python\Python310\python.exe
+
+print("Content-type: text/html\n\n")
+print("""
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <title>BornLikeThis - Home</title>
+      <style>
+      body{
+            font-family: Arial, sans-serif;
+      }
+      h2{
+            color: purple
+      }
+      </style>
+      </head>
+      <body>
+            <h2>Welcome to BornLikeThis - Home Page</h2>
+            <p>This is the Home Page content...</p>
+            <p><a href="about.py">About</a> | <a href="contact.py">Contact</a></p>
+      </body>
+      </html>
+      """)
