4d428493654ef484ed1ed2cff0e4ac05610e2fe1
silpa-2001 <161181980+silpa-2001@users.noreply.github.com>
2024-06-26 16:47:58 +0530

    Add files via upload

---
+++ contact.py
@@ -0,0 +1,24 @@
+#!C:\Users\Admin\AppData\Local\Programs\Python\Python310\python.exe
+
+print("Content-type: text/html\n\n")
+print("""
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <title>BornLikeThis - Contact</title>
+      <style>
+      body{
+            font-family: Arial, sans-serif;
+      }
+      h2{
+            color: purple
+      }
+      </style>
+      </head>
+      <body>
+            <h2>Welcome to BornLikeThis - Contact Page</h2>
+            <p>This is the Contact Page content...</p>
+            <p><a href="home.py">Home</a> | <a href="about.py">About</a></p>
+      </body>
+      </html>
+      """)
